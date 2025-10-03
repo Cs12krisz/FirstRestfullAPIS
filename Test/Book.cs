62b5455df90472cdca4199771bb303f8ac5cfc93
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-03 10:31:51 +0200

    Book class created

---
+++ Test/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Test
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime releaseDate { get; set; }
+    }
+}
